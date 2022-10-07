4c8add37d1acdc291d54ac3f64cd913e6e34e4ef
meganw0009 <oddprism01@gmail.com>
2022-10-07 12:54:28 -0500

    Added progress, uploading to firebase storage, and Toasts
    The code isn't perfect on my end. However, if a user selects an image, THEN uploads an image, it will always work. Note that firebase storage takes some time to load the new image.

--- app/src/main/java/com/example/firebasestorage_uploadandretrieveimages/MainActivity.java
+++ app/src/main/java/com/example/firebasestorage_uploadandretrieveimages/MainActivity.java
@@ -1,24 +1,45 @@
 package com.example.firebasestorage_uploadandretrieveimages;
+/*
+    Description: Used this tutorial to u
+    Link: https://www.youtube.com/watch?v=g2Iibnnqga0
+ */
+
 
 import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.firebasestorage_uploadandretrieveimages.databinding.ActivityMainBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
     Uri imageUri;
+    StorageReference storageReference;
+    ProgressDialog progressDialog;
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,9 +54,53 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        binding.uploadImageButton.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uploadImage();
+            }
+        }));
+    }
+    private void uploadImage() {
+
+        // Lets user know if file is being uploaded
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Uploading File...");
+        progressDialog.show();
+
+        // String in the form of year, month, day, hours, mins, and seconds
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss", Locale.US);
+        Date now = new Date();
+        String fileName = formatter.format(now);
+        storageReference = FirebaseStorage.getInstance().getReference("images/"+fileName);
+
+        storageReference.putFile(imageUri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        // don't want image in imagview as the image has been updated
+                        binding.firebaseimage.setImageURI(null);
+                        Toast.makeText(MainActivity.this, "Successfully Uploaded", Toast.LENGTH_SHORT).show();
+
+                        // Tell the user if successfully uploaded
+                        if (progressDialog.isShowing()){
+                            progressDialog.dismiss();
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                        // Dismiss progress if failed
+                        if (progressDialog.isShowing()){
+                            progressDialog.dismiss();
+                        }
+                        Toast.makeText(MainActivity.this, "Failed to Upload", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
 
     }
-
     private void selectImage() {
         Intent intent = new Intent();
         intent.setType("image/*");
